765eabd296a88ade471e21e589229868e520fd3d
Stephen Crane <sjc@immunant.com>
2019-02-25 17:12:50 -0800

    Update setup script to use c2rust --output-dir

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ plugin_args = ['-Xclang', '-plugin-arg-crosschecks',
                '-ffunction-sections',  # Used by --icf
                ]
 
+
 def c_files_in(directory):
     paths = []
     names = os.listdir(directory)
@@ -47,42 +48,6 @@ def process_gperf_file(gperf_file, output_file):
         raise Exception("Couldn't find `gperf`, is it installed?")
     subprocess.check_call(["gperf", gperf_file, "--output-file=%s" % output_file])
 
-'''
-extensions[0] -> extension for translating to rust
-extensions[1] -> extension for translating and running rust xcheck
-extensions[2] -> extension for running clang xcheck
-
-extensions = [
-    Extension(
-        name='snudown',
-        sources=['snudown.c', 'src/bufprintf.c'] + c_files_in('html/'),
-        include_dirs=['src', 'html'],
-        libraries=['c2rust_build'],
-        library_dirs=['c2rust-build/target/debug']
-    ),
-    Extension(
-        name='snudown',
-        sources=['snudown.c', 'src/bufprintf.c'] + c_files_in('html/'),
-        include_dirs=['src', 'html'],
-        #libraries=['c2rust_build', 'fakechecks'],
-        libraries=['c2rust_build', 'clevrbuf'],
-        library_dirs=['c2rust_build/target/debug', fakechecks_path, clevrbuf_path],
-        extra_link_args=['-Wl,-rpath,{},-rpath,{}'.format(fakechecks_path, clevrbuf_path)],
-    ),
-    Extension(
-        name='snudown',
-        sources=['snudown.c', 'xchecks/xchecks.c'] + c_files_in('src/') + c_files_in('html/'),
-        include_dirs=['src', 'html'],
-        library_dirs=[fakechecks_path, clevrbuf_path],
-        #libraries=["fakechecks"],
-        libraries=["clevrbuf"],
-        extra_compile_args=plugin_args,
-        extra_link_args=['-fuse-ld=gold', '-Wl,--gc-sections,--icf=safe',
-                            '-Wl,-rpath,{},-rpath,{}'.format(fakechecks_path, clevrbuf_path)],
-        extra_objects=[runtime_path],
-    ),
-]
-'''
 
 extensions = [
     Extension(
@@ -104,16 +69,16 @@ assert version
 
 class BuildSnudown(distutils.command.build.build):
     user_options = distutils.command.build.build.user_options + [
-    ('translate', None,
-    'translate from c to rust'),
+        ('translate', None,
+         'translate from c to rust'),
 
-    ('rust-crosschecks', None,
-    'translate then run rust crosschecks'),
-    ('clang-crosschecks', None,
-    'translate then run clang crosschecks'),
+        ('rust-crosschecks', None,
+         'translate then run rust crosschecks'),
+        ('clang-crosschecks', None,
+         'translate then run clang crosschecks'),
 
-    ('use-fakechecks', None,
-    'use the fakechecks library to print the cross-checks'),
+        ('use-fakechecks', None,
+         'use the fakechecks library to print the cross-checks'),
     ]
 
     def build_extension(self):
@@ -128,23 +93,23 @@ class BuildSnudown(distutils.command.build.build):
         extensions.pop()
         if self.translate is not None:
             sources.append('src/bufprintf.c')
-            library_dirs.append('c2rust-build/target/debug')
-            libraries.append('c2rust_build')
+            library_dirs.append('c2rust/target/debug')
+            libraries.append('c2rust')
 
         if self.rust_crosschecks is not None:
             sources.append('src/bufprintf.c')
-            library_dirs.extend(['c2rust-build/target/debug', fakechecks_path, clevrbuf_path])
+            library_dirs.extend(['c2rust/target/debug', fakechecks_path, clevrbuf_path])
             if self.use_fakechecks is not None:
-                libraries.extend(['c2rust_build', 'fakechecks'])
+                libraries.extend(['c2rust', 'fakechecks'])
             else:
-                libraries.extend(['c2rust_build', 'clevrbuf'])
+                libraries.extend(['c2rust', 'clevrbuf'])
             holder = ['-Wl,-rpath,{},-rpath,{}'.format(fakechecks_path, clevrbuf_path)]
             extra_link_args.extend(holder)
 
         if self.clang_crosschecks is not None:
             # Set the compiler path to cc_wrapper.sh
             os.environ["CC"] = "{cc_wrapper} {cc} {plugin}".format(
-                    cc_wrapper=cc_wrapper_path, cc=cc_path, plugin=plugin_path)
+                cc_wrapper=cc_wrapper_path, cc=cc_path, plugin=plugin_path)
             sources.append('xchecks/xchecks.c')
             sources.extend(c_files_in('src/'))
             library_dirs.extend([fakechecks_path, clevrbuf_path])
@@ -153,8 +118,9 @@ class BuildSnudown(distutils.command.build.build):
             else:
                 libraries.append('clevrbuf')
             extra_compile_args.extend(plugin_args)
-            extra_link_args.extend(['-fuse-ld=gold', '-Wl,--gc-sections,--icf=safe',
-                                '-Wl,-rpath,{},-rpath,{}'.format(fakechecks_path, clevrbuf_path)])
+            extra_link_args.extend([
+                '-fuse-ld=gold', '-Wl,--gc-sections,--icf=safe',
+                '-Wl,-rpath,{},-rpath,{}'.format(fakechecks_path, clevrbuf_path)])
             extra_objects.append(runtime_path)
 
         return Extension(
@@ -206,9 +172,10 @@ class BuildSnudown(distutils.command.build.build):
 
 class GPerfingBuildExt(build_ext):
     def run(self):
-        #translate.py builds this manually
-        #process_gperf_file("src/html_entities.gperf", "src/html_entities.h")
+        # translate.py builds this manually
+        # process_gperf_file("src/html_entities.gperf", "src/html_entities.h")
         build_ext.run(self)
+
 
 setup(
     name='snudown',
